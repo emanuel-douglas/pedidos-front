9cda55bc8124a2fc1d0c9fed52f1046b6fad1db2
Emanuel Douglas <email.emanueldouglas@gmail.com>
2025-06-16 21:22:00 -0300

    ïnvocacao da tela principal na classe main

--- src/main/java/com/vrsoftware/pedidos/Main.java
+++ src/main/java/com/vrsoftware/pedidos/Main.java
@@ -1,7 +1,9 @@
 package com.vrsoftware.pedidos;
 
+import com.vrsoftware.pedidos.view.PedidosFront;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        new PedidosFront().setVisible(true);
     }
 }
